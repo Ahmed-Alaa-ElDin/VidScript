2007b488c69fe95814230e7fd2e880fb8a0d0641
Ahmed Alaa ElDin <ahmed@amptive.com>
2025-04-17 15:22:16 +0200

    INIT :: Initialize the chrome extension

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("VidScribe Extension Installed.");
+});
---
+++ popup.js
@@ -0,0 +1,9 @@
+document.getElementById("extract").addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            alert("This will trigger OCR!");
+        },
+    });
+});
